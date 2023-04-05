8b705095968ebdcc0a24a757f89f23e4171ee777
sobol3 <>
2023-04-05 17:47:42 +0000

    addin dynamic data

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id": "1",
+        "title": "Junior",
+        "salary": "12.000$",
+        "location": "New York, USA"
+    },
+    {
+        "id": "2",
+        "title": "Regular",
+        "salary": "15.000$",
+        "location": "London, UK"
+    },
+    {
+        "id": "3",
+        "title": "Senior",
+        "salary": "22.000$",
+        "location": "Warsaw, Poland"
+    },
+]https://github.com/sobol3/job-offers
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html", jobs=JOBS)
 
 
 if __name__ == "__main__":
